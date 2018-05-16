a3824dc963bc236a5b14d012f7aa07327e193f4c
fatmanurBulacak <fatmanur.bulacak@gmail.com>
2018-05-16 09:14:04 +0300

    tests

---
+++ src/main/java/com/fatmanur/Junit.java
@@ -0,0 +1,31 @@
+package com.fatmanur;
+
+public class Junit {
+
+    public int square(int edgeA){
+
+        return edgeA*edgeA;
+    }
+
+    public int sum(int number1,int number2) {
+
+        return number1 + number2;
+    }
+
+    public int sub(int number1,int number2) {
+
+        return number1 - number2;
+    }
+
+    public String addWord(String a, String b){
+
+        return a+b;
+    }
+
+    public boolean ciftMiDogrula(double sayi) {
+
+        return sayi%2 == 0;
+    }
+
+
+}
---
+++ src/test/java/com/fatmanur/JunitTest.java
@@ -0,0 +1,53 @@
+package com.fatmanur;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JunitTest {
+
+    @Test
+    public void testSquare(){
+
+        Junit testSquare = new Junit();
+        int output=testSquare.square(5);
+        Assert.assertEquals(25,output);
+
+    }
+
+    @Test
+    public void testSum(){
+        Junit testSum = new Junit();
+        int result = testSum.sum(5,2);
+        Assert.assertEquals("Toplama islemi hatali",7,result);
+
+    }
+
+    @Test
+    public void testSub(){
+        Junit testSub = new Junit ();
+        int result= testSub.sub(12,7);
+        Assert.assertEquals("Cikarma islemi hatali",5,result);
+
+
+    }
+
+
+     @Test
+    public void testAddWord(){
+
+        Junit addword=new Junit();
+        String result = addword.addWord("hello","world");
+        Assert.assertEquals("helloworld",result);
+     }
+
+     //sor
+    @Test
+    public void testciftMi(){
+
+        Junit ciftSayiMi = new Junit();
+        boolean result = ciftSayiMi.ciftMiDogrula(8);
+        Assert.assertTrue("çiftdeğil",false);
+    }
+
+
+}
